adad28817d7a3687e53bbbbbac91318af34c1b20
Cynthia Pardede <62656001+Html19@users.noreply.github.com>
2022-08-24 10:46:59 +0700

    add mama

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public class hello {
         System.out.println("Hello, World!"); 
         System.out.println("Hello, Cynthia");
         System.out.println("Hello, Dunia");
+        System.out.println("Hello, Mama");
     }
 }
